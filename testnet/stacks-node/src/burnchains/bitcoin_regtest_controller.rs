7576d6f9e225f83efabb1f03377b3ea0b74ba1ba
SherLzp <lzp.sher@gmail.com>
2021-01-21 19:23:34 +0800

    [Sher] send transaction

--- testnet/stacks-node/src/burnchains/bitcoin_regtest_controller.rs
+++ testnet/stacks-node/src/burnchains/bitcoin_regtest_controller.rs
@@ -1234,8 +1234,8 @@ impl BurnchainController for BitcoinRegtestController {
         };
 
         println!("进入发送交易");
-        false
-//        self.send_transaction(transaction)
+//        false
+        self.send_transaction(transaction)
     }
 
     #[cfg(test)]
